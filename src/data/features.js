73756cbafd304f9c28cc14c9d074a3d0041b0ac2
Danny Salman <salman.danny03@hotmail.com>
2022-05-09 00:38:45 -0400

    modify landing page cards and update directs

--- src/data/features.js
+++ src/data/features.js
@@ -3,23 +3,8 @@ export const firstRow = [
         title: "Polygon PoS",
         linkUrl: "docs/home/new-to-polygon",
         imageUrl: "img/home/blockchain-basics.svg",
-        description: "Learn about Polygon’s scalable proof-of-stake architecture, blockchain basics, and the three types of security models."
+        description: "Polygon PoS is a Layer 2 scaling solution for Ethereum-based public blockchains."
     },
-    {
-        title: "Develop",
-        linkUrl: "docs/develop/getting-started",
-        imageUrl: "img/develop.png",
-        description: "Learn how to build your blockchain application and how to deploy on Polygon PoS."
-    },
-    {
-        title: "Validate",
-        linkUrl: "docs/maintain/validate/getting-started",
-        imageUrl: "img/validate.png",
-        description: "Participate in securing the network by providing computing resources to Polygon PoS chain. Learn how to operate your own node and how to stake on Polygon."
-    },
-];
-
-export const secondRow = [
     {
         title: "Polygon Edge",
         class_name: "polygon-banner",
@@ -28,28 +13,49 @@ export const secondRow = [
         description: "A modular and extensible framework for building Ethereum-compatible blockchain networks."
     },
     {
-        title: "Polygon Avail",
+        title: "Polygon Supernets: WIP",
         linkUrl: "",
-        imageUrl: "img/avail.svg",
-        description: 'A general-purpose, scalable, data availability-focused blockchain. Avail can allocate data transactions in a scalable manner and check whether the stored data is available. '
+        imageUrl: "img/supernet.svg",
+        description: "Application-specific chains that are interoperable, scalable & secure, powered by Polygon Edge."
     },
+];
+
+export const secondRow = [
+    {
+        title: "Develop a Dapp",
+        linkUrl: "docs/develop/getting-started",
+        imageUrl: "img/develop.png",
+        description: "Build your blockchain application and deploy on the Polygon PoS."
+    },
+    {
+        title: "Operate a Node",
+        linkUrl: "docs/maintain/validate/getting-started",
+        imageUrl: "img/validate.png",
+        description: "Secure the network by staking and operating a node on the Polygon PoS chain."
+    },
+    {
+        title: "Integrate with Polygon",
+        linkUrl: "docs/contribute/orientation",
+        imageUrl: "img/home/integration.svg",
+        description: "Integrate your project, tool, or solution with the Polygon PoS chain."
+    },
+];
+
+export const thirdRow = [
     {
         title: "Polygon Zero",
         linkUrl: "",
         imageUrl: "img/zero.svg",
         description: "A highly scalable, Ethereum-compatiable zk-rollup."
     },
-];
-
-export const thirdRow = [
     {
         title: "Polygon Hermez",
-        linkUrl: "",
+        linkUrl: "https://docs.hermez.io/",
         imageUrl: "img/hermez.svg",
         description: "A zk-rollup solution for scaling payments and token transfers."
     },
     {
-        title: "Polygon Miden",
+        title: "Polygon Miden: WIP",
         linkUrl: "",
         imageUrl: "img/miden.svg",
         description: "A STARK-based zk-rollup solution that supports arbitrary smart contracts"
@@ -61,18 +67,17 @@ export const thirdRow = [
         description: "Nightfall is an Optimist Rollup aimed to lower the transactions cost of ERC20, ERC721 and ERC1155 token private transfers."
     },
     {
-        title: "Polygon Supernets",
+        title: "Polygon Avail",
         linkUrl: "",
-        imageUrl: "img/supernet.svg",
-        description: "Application-specific chains that are interoperable, scalable & secure, powered by Polygon Edge."
+        imageUrl: "img/avail.svg",
+        description: 'A general-purpose, scalable, data availability-focused blockchain. Avail can allocate data transactions in a scalable manner and check whether the stored data is available. '
     },
     {
-        title: "Polygon ID",
+        title: "Polygon ID: WIP",
         linkUrl: "",
         imageUrl: "img/polygon-id.svg",
         description: "A blockchain-native identity and authentication solution."
     },
-
 ];
 
 export const FourthRow = [
