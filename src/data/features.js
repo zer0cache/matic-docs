1360db833c7fabe04f4b3dcefd57401b7cf908fe
Nazeim <nazemfaour89@gmail.com>
2022-05-16 16:53:17 +0300

    Apply tabs structure

--- src/data/features.js
+++ src/data/features.js
@@ -1,36 +1,45 @@
-export const firstRow = [
-    {
-        title: "Quick Start",
-        linkUrl: "docs/home/new-to-polygon/",
-        imageUrl: "img/supernet.svg",
-        description: "Learn about the basics of blockchain and Polygon and how to get started."
-    },
-    {
-        title: "Delegate MATIC",
-        linkUrl: "docs/maintain/delegate/delegate/",
-        imageUrl: "img/supernet.svg",
-        description: "Secure the network by staking MATIC tokens & delegate your stake to Polygon validators."
-    },
-    {
-        title: "Participate in Governance: Coming Soon",
-        linkUrl: "docs/maintain/govern/governance",
-        imageUrl: "img/supernet.svg",
-        description: "Vote on proposals and participate in the Polygon DAO."
-    },
+export const firstRowBeginner = [
+  {
+      title: "Quick Start",
+      status: "Beginner",
+      linkUrl: "docs/home/new-to-polygon/",
+      imageUrl: "img/supernet.svg",
+      description: "Learn about the basics of blockchain and Polygon and how to get started."
+  },
+  {
+      title: "Delegate MATIC",
+      status: "Beginner",
+      linkUrl: "docs/maintain/delegate/delegate/",
+      imageUrl: "img/supernet.svg",
+      description: "Secure the network by staking MATIC tokens & delegate your stake to Polygon validators."
+  },
+  {
+      title: "Participate in Governance: Coming Soon",
+      status: "Beginner",
+      linkUrl: "docs/maintain/govern/governance",
+      imageUrl: "img/supernet.svg",
+      description: "Vote on proposals and participate in the Polygon DAO."
+  },
+];
+
+export const firstRowAdvanced = [
     {
         title: "Develop a dApp",
+        status: "Advanced",
         linkUrl: "docs/develop/getting-started",
         imageUrl: "img/home/dapp-developers.svg",
         description: "Build an application and deploy on Polygon."
     },
     {
         title: "Operate a Node",
+        status: "Advanced",
         linkUrl: "docs/maintain/validate/validator-index/",
         imageUrl: "img/home/core-contribution.svg",
         description: "Secure the network by staking and operating a node to validate on Polygon."
     },
     {
         title: "Integrate a Solution",
+        status: "Advanced",
         linkUrl: "docs/integrate/quickstart",
         imageUrl: "img/home/integration.svg",
         description: "Integrate an application, tool, wallet, oracle, and more with Polygon."
@@ -39,56 +48,65 @@ export const firstRow = [
 
 export const secondRow = [
     {
-        title: "Polygon PoS v3: Coming Soon",
+        title: "Polygon PoS",
+        status: "V3: Coming Soon",
         linkUrl: "",
         imageUrl: "img/home/blockchain-basics.svg",
         description: "A Layer 2 proof of stake scaling solution for Ethereum-based public blockchains."
     },
     {
         title: "Polygon Edge",
+        status: "SDK",
         class_name: "polygon-banner",
         linkUrl: "https://sdk-docs.polygon.technology/docs/overview/",
         imageUrl: "img/home/polygon-sdk.svg",
         description: "A modular and extensible framework for building Ethereum-compatible blockchain networks."
     },
     {
-        title: "Polygon Supernets: Coming Soon",
+        title: "Polygon Supernets",
+        status: "Coming Soon",
         linkUrl: "",
         imageUrl: "img/supernet.svg",
         description: "Application-specific chains that are interoperable, scalable & secure: powered by Polygon Edge."
     },
     {
         title: "Polygon Avail",
+        status: "Data Availability",
         linkUrl: "https://avail-docs.matic.today/",
         imageUrl: "img/avail.svg",
         description: 'A general-purpose, scalable, data availability-focused blockchain.'
     },
     {
         title: "Polygon Nightfall",
+        status: "Optimistic Rollup",
         linkUrl: "docs/nightfall/introduction/overview",
         imageUrl: "img/Nightfall.png",
         description: "An optimistic rollup solution designed for enterprises that supports private transactions."
     },
     {
         title: "Polygon Hermez",
+        status: "Zk Rollup",
         linkUrl: "https://docs.hermez.io/",
         imageUrl: "img/hermez.svg",
         description: "A zk-rollup solution for scaling payments and token transfers."
     },
     {
-        title: "Polygon Miden: Coming Soon",
+        title: "Polygon Miden",
+        status: "STARK-based Zk Rollup: Coming Soon",
         linkUrl: "",
         imageUrl: "img/miden.svg",
         description: "A STARK-based zk-rollup solution that supports arbitrary smart contracts."
     },
     {
-        title: "Polygon Zero: Coming Soon",
+        title: "Polygon Zero",
+        status: "Zk Rollup: Coming Soon",
         linkUrl: "",
         imageUrl: "img/zero.svg",
         description: "A highly scalable, Ethereum-compatiable zk-rollup."
     },
     {
-        title: "Polygon ID: Coming Soon",
+        title: "Polygon ID",
+        status: "Identity & Authentication: Coming Soon",
         linkUrl: "",
         imageUrl: "img/polygon-id.svg",
         description: "A blockchain-native identity and authentication solution."
