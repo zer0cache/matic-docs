acd02e1e5fc03826d04626666e2b8d4d0e1513c5
Danny Salman <salman.danny03@hotmail.com>
2022-08-24 02:00:31 -0400

    CON-92, CON-98: Add Avail Consensus (#974)
    * avail consensus init
    
    * address SME feedback

--- sidebars.js
+++ sidebars.js
@@ -865,6 +865,7 @@ module.exports = {
   avail: [
     "avail/introduction/what-is-avail",
     "avail/architecture/avail-system-overview",
+    "avail/architecture/avail-consensus",
     "avail/how-tos/avail-quick-start",
     "avail/node/avail-node-management",
     "avail/faq",
